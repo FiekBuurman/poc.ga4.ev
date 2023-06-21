6b026532ec5ebf041d1f6ff80f10d224f31f6ae4
Fiek Buurman <fiek@marti-orbak.nl>
2023-06-21 08:52:24 +0200

    just some temp model and data, will be replcaed with the actual OrderEntity in EV

--- Models/OrderEntity.cs
+++ Models/OrderEntity.cs
@@ -1,7 +1,34 @@
-﻿namespace poc.ga4.ev.Models
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace poc.ga4.ev.Models
 {
     internal class OrderEntity
     {
         // TODO this is gonna be the EV OrderEntity
-    }
+
+		public List<OrderItemEntity> OrderItems => new()
+		{
+			new OrderItemEntity
+			{
+				OrderItemId = 123,
+				ProductName = "product123",
+				Price = 1.23m
+			},
+			new OrderItemEntity
+			{
+				OrderItemId = 456,
+				ProductName = "product456",
+				Price = 4.56m
+			}
+		};
+	}
+
+	internal class OrderItemEntity
+	{
+        public int OrderItemId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+		public int Quantity { get; set; }
+	}
 }
