783b73b47fd28f11935604b769e047a2fdf8a84f
Fiek Buurman <fiek@marti-orbak.nl>
2023-06-23 12:15:55 +0200

    added few more properties to the test model

--- Models/OrderEntity.cs
+++ Models/OrderEntity.cs
@@ -3,9 +3,13 @@ using System.Reflection.Emit;
 
 namespace poc.ga4.ev.Models
 {
-    internal class OrderEntity
-    {
-        // TODO this is gonna be the EV OrderEntity
+	internal class OrderEntity
+	{
+		// TODO this is gonna be the EV OrderEntity
+
+		public string Number { get; set; }
+		public decimal TotalPrice { get; set; }
+		public decimal DeliveryCost { get; set; }
 
 		public List<OrderItemEntity> OrderItems => new()
 		{
@@ -22,6 +26,23 @@ namespace poc.ga4.ev.Models
 				Price = 4.56m
 			}
 		};
+
+		public CouponEntity CouponEntity { get; set; }
+		public CustomerEntity CustomerEntity { get; set; }
+
+		public string  DeliveryMethod { get; set; }
+		public string PaymentMethod { get; set; }
+	}
+
+	internal class CustomerEntity
+	{
+		public string LastName { get; set; } = "Marti-Orbak";
+		public string Address { get; set; } = "Hollewatering 3b";
+	}
+
+	internal class CouponEntity
+	{
+		public string CouponName { get; set; } = "Zomerkorting";
 	}
 
 	internal class OrderItemEntity
