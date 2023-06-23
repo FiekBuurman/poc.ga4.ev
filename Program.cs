cc07c4b78f875a6a99144223b3ac1518a307a76b
Fiek Buurman <fiek@marti-orbak.nl>
2023-06-23 12:54:07 +0200

    create jsons for all events

--- Program.cs
+++ Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using poc.ga4.ev.EventModels;
+using poc.ga4.ev.Factories;
+using poc.ga4.ev.Models;
+using poc.ga4.ev.Types;
 
 namespace poc.ga4.ev
 {
     internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+	{
+		private static string outputDirectory;
+		static void Main(string[] args)
+		{
+			outputDirectory = GetDirectory();
+
+			var orderEntity = new OrderEntity();
+			var factory = new EventModelFactory();
+			var eventTypes = Enum.GetValues(typeof(EventType));
+
+			foreach (EventType eventType in eventTypes)
+			{
+				var eventModel = factory.Create(eventType, orderEntity);
+				WriteToJsonFile(eventModel);
+			}
+		}
+
+		private static void WriteToJsonFile(BaseEventModel baseEventModel)
+		{
+			var jsonString = JsonConvert.SerializeObject(baseEventModel);
+			File.WriteAllText($"{outputDirectory}{baseEventModel.GetType().Name}.json", jsonString);
         }
-    }
+
+		private static string GetDirectory()
+		{
+			var directoryPath = $"d:/Develop/Repos/poc.ga4.ev/examples/{DateTime.Now:yyyyMMddHHmmss}/";
+			return Directory.CreateDirectory(directoryPath).FullName;
+		}
+	}
 }
